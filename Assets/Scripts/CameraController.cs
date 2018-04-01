65f17884bb88defcc687733067516931f48fe9a5
Unknown <tassinariz@student.elms.edu>
2018-03-31 21:18:50 -0400

    Updated Speeds

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,9 @@ public class CameraController : MonoBehaviour {
 
     public Transform player;
 
+    //public float wallsZ;
+    //public float wallsX;
+
     private Vector3 offset;
 
 	// Use this for initialization
@@ -13,6 +16,8 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame, but LateUpdate is always called LAST.
 	void LateUpdate () {
+
+        //transform.position.x = Mathf.Clamp(transform.position.x, minPosition, maxPosition);
         transform.position = player.position + offset; // every frame, right before the camera updates, the camera's postion is set to the player's position + the offset.
 	}
 }
